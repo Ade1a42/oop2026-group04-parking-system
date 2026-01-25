4a4e43be5736bce3b0d4db2aa1e828c3121043dd
Amanov Alikhan <amanovali571@gmail.com>
2026-01-25 15:43:24 +0500

    IDB intefrace addes,updated DatabaseConnection to implement IDB

--- src/main/parking/database/DatabaseConnection.java
+++ src/main/parking/database/DatabaseConnection.java
@@ -4,15 +4,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseConnection {
+abstract class DatabaseConnection implements IDB {
     private static final String URL = "jdbc:postgresql://aws-1-eu-west-1.pooler.supabase.com:5432/postgres?sslmode=require";
     private static final String USER = "postgres.nvikiacxeckhymdasttx";
     private static final String PASSWORD = "Smart_parking_2026";
 
-    private DatabaseConnection() {
+    private static DatabaseConnection instance;
+
+    private DatabaseConnection() {} // Private constructor for Singleton
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
     }
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:postgresql://aws-1-eu-west-1.pooler.supabase.com:5432/postgres?sslmode=require", "postgres.nvikiacxeckhymdasttx", "Smart_parking_2026");
+    @Override
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
