fef5b0dc7c88e0dc0dc3450cc2e420e9571e8a21
Asel <addel4242@gmail.com>
2026-01-25 13:00:58 +0500

    deleted error files to start over

--- src/main/parking/database/IDB.java
+++
@@ -1,9 +0,0 @@
-package parking.database;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface IDB {
-    Connection getConnection() throws SQLException;
-    void closeConnection(Connection connection);
-}
